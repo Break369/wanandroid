57c8fd13c3a5518433b9341ab369f4340c1627ff
1298587604@qq.com <good05300511>
2020-10-06 23:00:06 +0800

    测试提交

---
+++ app/src/main/java/luyao/wanandroid/ui/test/TestFragment.kt
@@ -0,0 +1,24 @@
+package luyao.wanandroid.ui.test
+
+import luyao.mvvm.core.base.BaseVMFragment
+import luyao.wanandroid.R
+import luyao.wanandroid.databinding.FragmentTestBinding
+
+/**
+ * @author: CaiSongL
+ * @date: 2020/10/5 22:12
+ */
+class TestFragment : BaseVMFragment<FragmentTestBinding>(R.layout.fragment_test) {
+
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initData() {
+        TODO("Not yet implemented")
+    }
+
+    override fun startObserve() {
+        TODO("Not yet implemented")
+    }
+}
